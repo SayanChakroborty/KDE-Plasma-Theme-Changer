bcfec864795a754149f1c9f18cd9b80abb0fe6c9
Sayan Chakroborty <sayan@e.email>
2020-12-26 18:35:26 +0530

    Update LightMode.sh

--- LightMode.sh
+++ LightMode.sh
@@ -1,13 +1,11 @@
-#! /bin/zsh
-
 lookandfeeltool --apply org.kde.breeze.desktop;
 
-sleep 5;
+kquitapp5 plasmashell;
 
-kquitapp5 plasmashell && kwin_x11 --replace & kstart5 plasmashell;
+kwin_x11 --replace &;
 
-sleep 5;
+kstart5 plasmashell;
 
 konsoleprofile colors=Breeze Light;
 
-sed -i 's/DefaultProfile=BreezeDark.profile/DefaultProfile=BreezeLight.profile/g' /home/ron/.config/konsolerc;
+sed -i 's/DefaultProfile=BreezeDark.profile/DefaultProfile=BreezeLight.profile/g' ~/.config/konsolerc;
