0a726ad67cdea9959a7e13c2144337b321cf9c7d
Sayan Chakroborty <sayan@e.email>
2020-12-26 18:37:23 +0530

    Update DarkMode.sh

--- DarkMode.sh
+++ DarkMode.sh
@@ -1,14 +1,11 @@
-#! /bin/zsh
-
 lookandfeeltool --apply org.kde.breezedark.desktop;
 
-sleep 5;
+kquitapp5 plasmashell;
 
-kquitapp5 plasmashell && kwin_x11 --replace & kstart5 plasmashell;
+kwin_x11 --replace &;
 
-sleep 5;
+kstart5 plasmashell;
 
 konsoleprofile colors=Breeze;
 
-sed -i 's/DefaultProfile=BreezeLight.profile/DefaultProfile=BreezeDark.profile/g' /home/ron/.config/konsolerc;
- 
+sed -i 's/DefaultProfile=BreezeLight.profile/DefaultProfile=BreezeDark.profile/g' ~/.config/konsolerc;
