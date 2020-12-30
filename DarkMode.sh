dde9de093bd1105764627bcf69060420c661fdfa
Sayan Chakroborty <sayan@e.email>
2020-12-30 13:37:26 +0530

    Delete DarkMode.sh

--- DarkMode.sh
+++
@@ -1,9 +0,0 @@
-lookandfeeltool --apply org.kde.breezedark.desktop;
-
-kquitapp5 plasmashell && kstart5 plasmashell;
-
-kwin_x11 --replace &;
-
-konsoleprofile colors=Breeze;
-
-sed -i 's/DefaultProfile=Day.profile/DefaultProfile=Night.profile/g' ~/.config/konsolerc;
