152422131fbedd904e21e53cd7463a4c488738e6
Sayan Chakroborty <sayan@e.email>
2020-12-30 13:37:17 +0530

    Delete LightMode.sh

--- LightMode.sh
+++
@@ -1,9 +0,0 @@
-lookandfeeltool --apply org.kde.breeze.desktop;
-
-kquitapp5 plasmashell && kstart5 plasmashell;
-
-kwin_x11 --replace &;
-
-konsoleprofile colors="Breeze Light";
-
-sed -i 's/DefaultProfile=Night.profile/DefaultProfile=Day.profile/g' ~/.config/konsolerc;
